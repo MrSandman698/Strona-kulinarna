375e155781b377207a10202c659875398a704715
Emil5321 <mackowiakowemil@gmail.com>
2023-11-07 21:05:59 +0100

    added polish forms

--- SzukajPoSkladnikach.php
+++ SzukajPoSkladnikach.php
@@ -45,46 +45,46 @@
         <p class="header">Składniki jakie masz w domu:</p>
         <div class="checklist">
         <label class="container">makaron
-          <input type="checkbox" id="składnik1" name="sk[]" value="makaron">
+          <input type="checkbox" id="składnik1" name="sk[]" value="makar">
           <span class="checkmark"></span>
         </label><label class="container">jajka
-          <input type="checkbox" id="składnik2" name="sk[]" value="jajka">
+          <input type="checkbox" id="składnik2" name="sk[]" value="jaj">
           <span class="checkmark"></span>
         </label>
         <label class="container">cebula
-          <input type="checkbox" id="składnik3" name="sk[]" value="cebula">
+          <input type="checkbox" id="składnik3" name="sk[]" value="cebul">
           <span class="checkmark"></span>
         </label>
         <label class="container">pomidor
-          <input type="checkbox" id="składnik4" name="sk[]" value="pomidor">
+          <input type="checkbox" id="składnik4" name="sk[]" value="pomi">
           <span class="checkmark"></span>
         </label>
         <label class="container">marchewka
-          <input type="checkbox" id="składnik5" name="sk[]" value="marchewka">
+          <input type="checkbox" id="składnik5" name="sk[]" value="marche">
           <span class="checkmark"></span>
         </label>
         <label class="container">pietruszka
-          <input type="checkbox" id="składnik6" name="sk[]" value="pietruszka">
+          <input type="checkbox" id="składnik6" name="sk[]" value="pietru">
           <span class="checkmark"></span>
         </label>
         <label class="container">kurczak
-          <input type="checkbox" id="składnik7" name="sk[]" value="kurczak">
+          <input type="checkbox" id="składnik7" name="sk[]" value="kurcz">
           <span class="checkmark"></span>
         </label>
         <label class="container">schab
-          <input type="checkbox" id="składnik8" name="sk[]" value="schab">
+          <input type="checkbox" id="składnik8" name="sk[]" value="scha">
           <span class="checkmark"></span>
         </label>
         <label class="container">baranina
-          <input type="checkbox" id="składnik9" name="sk[]" value="baranina">
+          <input type="checkbox" id="składnik9" name="sk[]" value="barani">
           <span class="checkmark"></span>
         </label>
         <label class="container">czosnek
-          <input type="checkbox" id="składnik10" name="sk[]" value="czosnek">
+          <input type="checkbox" id="składnik10" name="sk[]" value="czosn">
           <span class="checkmark"></span>
         </label>
         <label class="container">sałata
-          <input type="checkbox" id="składnik11" name="sk[]" value="sałata">
+          <input type="checkbox" id="składnik11" name="sk[]" value="sałat">
           <span class="checkmark"></span>
         </label>
         <label class="container">ser
@@ -92,11 +92,11 @@
           <span class="checkmark"></span>
         </label>
         <label class="container">ogórek
-          <input type="checkbox" id="składnik13" name="sk[]" value="ogórek">
+          <input type="checkbox" id="składnik13" name="sk[]" value="ogór">
           <span class="checkmark"></span>
         </label>
           <label class="container">burak
-            <input type="checkbox" id="składnik14" name="sk[]" value="burak">
+            <input type="checkbox" id="składnik14" name="sk[]" value="bura">
             <span class="checkmark"></span>
           </label>
         </div>
